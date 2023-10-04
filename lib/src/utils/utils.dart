c98196cdeb62968763669c40bccdbe919d24adda
Babak <babakcode@gmail.com>
2023-10-04 18:50:21 +0330

    change const to var when declare id for o3d

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -7,7 +7,7 @@ class Utils {
       'babakcode-${random.nextInt(99999999)}-${random.nextInt(99999999)}';
 
   String relatedJs({required String id}) =>
-      """const mainModelViewer = document.querySelector('#$id');
+      """var mainModelViewer = document.querySelector('#$id');
 cameraOrbit = (a, b, c) => {mainModelViewer.cameraOrbit = `\${a}deg \${b}deg \${c}mm`}
 cameraTarget = (x, y, z) => {mainModelViewer.cameraTarget = `\${x}mm \${y}mm \${z}mm`}
 customEvaluate = (code) => { eval(code) }
